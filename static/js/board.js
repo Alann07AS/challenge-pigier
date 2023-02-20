3501d3c21ceda61826e3072392d57d82176e95e1
Alann <SchnebelenAlann@hotmail.com>
2023-02-20 11:50:02 +0100

    firstcommit

---
+++ static/js/board.js
@@ -0,0 +1,13 @@
+console.log("INIT BOARD CLASS");
+
+
+class board {
+    constructor (name) {
+        this.name = name;
+        this.hardway = [];
+        this.easyway = [];
+        this.inForkId = Math.random(4)+1; //choisis alétoirement la case de bifurcation
+        this.outForkId = this.InForkId+2;   //récuperer la bifurcation 2 case aprés
+        
+    }
+}
