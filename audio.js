718b5eb1d82f5e23bfe761af829a5623efb32457
Alann <SchnebelenAlann@hotmail.com>
2023-02-24 11:37:04 +0100

    AUDIO

---
+++ audio.js
@@ -0,0 +1,11 @@
+ class Son {
+    static bonnereponse    = new Audio("./audio/correct.wav")
+
+    static mauvaisereponse = new Audio("./audio/wrong.wav")
+
+    static applaudissement = new Audio("./audio/applause.mp3")
+    
+    static carte           = new Audio("./audio/flipcard.mp3")
+
+    static pion            = new Audio("./audio/pawn.mp3")
+}
